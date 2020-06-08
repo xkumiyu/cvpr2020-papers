6b4db0dc490249675d1003612a89b4dfff883ac3
xkumiyu <xkumiyu@gmail.com>
2020-06-08 21:50:57 +0900

    add session

--- scripts/main.py
+++ scripts/main.py
@@ -7,11 +7,18 @@ URL = "http://cvpr2020.thecvf.com/program/main-conference"
 
 def get_data_from_table(table):
     data = []
+    session = table.getprevious().text_content().replace("Session: ", "")
     for tr in table.findall("tbody/tr"):
         row = [td.text for td in tr.findall("td")]
         if len(row) == 6:
             data.append(
-                {"Paper ID": row[5], "Paper Title": row[3], "Author(s)": row[4]}
+                {
+                    "Paper ID": row[5],
+                    "Paper Title": row[3],
+                    "Author(s)": row[4],
+                    "Session": session,
+                    "Oral" : session[:4] == "Oral"
+                }
             )
     return data
 
