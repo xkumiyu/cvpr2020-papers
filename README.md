c5bbc04bb7daa874f0c8ba72b3a0f6da82ef3933
xkumiyu <xkumiyu@gmail.com>
2020-06-08 16:46:46 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# CVPR 2020 Paper
+
+[CVPR 2020 Paper List](papers.csv) in CSV format.
---
+++ scripts/main.py
@@ -0,0 +1,32 @@
+import lxml.html
+import pandas as pd
+import requests
+
+URL = "http://cvpr2020.thecvf.com/program/main-conference"
+
+
+def get_data_from_table(table):
+    data = []
+    for tr in table.findall("tbody/tr"):
+        row = [td.text for td in tr.findall("td")]
+        if len(row) == 6:
+            data.append(
+                {"Paper ID": row[5], "Paper Title": row[3], "Author(s)": row[4]}
+            )
+    return data
+
+
+def main():
+    res = requests.get(URL)
+    html = lxml.html.fromstring(res.text)
+
+    data = []
+    for table in html.xpath("//table"):
+        data += get_data_from_table(table)
+
+    df = pd.DataFrame(data).set_index("Paper ID")
+    df.to_csv("papers.csv")
+
+
+if __name__ == "__main__":
+    main()
